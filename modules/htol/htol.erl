f616ec97a2b151d99236f4372d12fba40b82307f
Mahesh Baheti <mahsh.baheti@gmail.com>
2015-09-19 23:00:18 +0900

    Add Heathrow to London problem solution.

---
+++ modules/htol/htol.erl
@@ -0,0 +1,37 @@
+-module(htol).
+-compile(export_all).
+
+main([Filename]) ->
+  {ok, Bin} = file:read_file(Filename),
+  Map = parse_map(Bin),
+  io:format("~p~n",[optimal_path(Map)]),
+  erlang:halt().
+
+% A function to parse the given file contents into a list of
+% [{A1,B1,X1},{A2,B2,X2},..]
+parse_map(Bin) when is_binary(Bin) ->
+  parse_map(binary_to_list(Bin));
+parse_map(Str) when is_list(Str) ->
+  Values = [list_to_integer(X) || X <- string:tokens(Str, "r\n\t ")],
+  group_vals(Values, []).
+
+% A function to change [A1,B1,X1,A2,B2,X2..] into grouped values
+% [{A1,B1,X1}, {A2,B2,X2}...]
+group_vals([], Acc) ->
+  lists:reverse(Acc);
+group_vals([A,B,X|Rest], Acc) ->
+  group_vals(Rest, [{A,B,X} | Acc]).
+
+shortest_step({A,B,X}, {{DistA,PathA}, {DistB,PathB}}) ->
+  OptA1 = {DistA + A, [{a,A}|PathA]},
+  OptA2 = {DistB + B + X, [{x,X}, {b,B}|PathB]},
+  OptB1 = {DistB + B, [{b,B}|PathB]},
+  OptB2 = {DistA + A + X, [{x,X}, {a,A}|PathA]},
+  {erlang:min(OptA1,OptA2), erlang:min(OptB1, OptB2)}.
+
+optimal_path(Map) ->
+  {A,B} = lists:foldl(fun shortest_step/2, {{0, []}, {0, []}}, Map),
+  {_Dist, Path} = if hd(element(2,A)) =/= {x,0} -> A;
+                     hd(element(2,B)) =/= {x,0} -> B
+                  end,
+  lists:reverse(Path).
